f38975b96fbe320c798964c13c5caa7a1eeb5f65
Avinash kumar <avinashkumarsingh9211@gmail.com>
2024-04-09 14:41:16 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+        RWOnly rwOnly=new RWOnly();
+       // rwOnly.name="Avinash kumar";
+        //compile time error
+        rwOnly.setName("Avinash kumar");
+        System.out.print(rwOnly.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class RWOnly {
-    
+   private String name;
+
+
+    public String  setName(String name) {
+        return this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
+
+
+
+
